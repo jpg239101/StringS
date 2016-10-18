e3d4d5fdde08347c2844dbef2c23baa51eaf37b1
jg101 <jpg239101@gmail.com>
2016-10-18 14:08:40 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // public boolean startsWith(String ss)
+        //This method returns true or false after testing the beginning of a string with the string given to it
+        String b = "word";
+        System.out.println(b.startsWith("Wor"));
+    }
+}
